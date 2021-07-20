d5be1fcd958278d786c2eb459fe7aad990318668
SASUPERNOVA <agentrip@gmail.com>
2021-07-20 12:54:08 +0300

    Implement onExitButtonClick and onConfirm methods in AddCategoryModal component

--- src/components/AddCategoryModal/AddCategoryModal.js
+++ src/components/AddCategoryModal/AddCategoryModal.js
@@ -6,6 +6,23 @@
 
         async connectedCallback() {
             await super.connectedCallback();
+            this.props = {
+                exitButton: this.shadowRoot.querySelector('#exit-button'),
+                modalInput: this.shadowRoot.querySelector('#modal-input'),
+                confirmButton: this.shadowRoot.querySelector('#confirm-button')
+            }
+
+            this.props.exitButton.addEventListener('click', (_ev) => this.onExitButtonClick());
+            this.props.confirmButton.addEventListener('click', (_ev) => this.onConfirm());
+        }
+
+        onExitButtonClick() {
+            this.remove();
+        }
+
+        onConfirm() {
+            this.hostComponent().dispatchEvent(new CustomEvent('add-category-modal-confirm', {detail: this.props.modalInput.value}));
+            this.remove();
         }
     }
 
