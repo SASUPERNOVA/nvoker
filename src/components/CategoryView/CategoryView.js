835a840014dd63b805846d94a162e7d530c8a803
SASUPERNOVA <agentrip@gmail.com>
2021-07-20 12:07:17 +0300

    Add toggleModal method in CategoryView component

--- src/components/CategoryView/CategoryView.js
+++ src/components/CategoryView/CategoryView.js
@@ -6,6 +6,18 @@
 
         async connectedCallback() {
             await super.connectedCallback();
+            this.shadowRoot.querySelector('#add-category-button').addEventListener('click', this.toggleModal);
+        }
+
+        toggleModal() {
+            const modal = document.querySelector('add-category-modal');
+            if (modal) {
+                modal.remove();
+            }
+            else {
+                const addCategoryModal = document.createElement('add-category-modal');
+                document.querySelector('#modal-root').appendChild(addCategoryModal);
+            }
         }
     }
 
