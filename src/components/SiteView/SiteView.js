4ead11e75c9f464ebb51bdda44624be0a8c4458a
SASUPERNOVA <agentrip@gmail.com>
2021-07-22 05:42:18 +0300

    Add showAddModal and onAddModalConfirm methods in SiteView component

--- src/components/SiteView/SiteView.js
+++ src/components/SiteView/SiteView.js
@@ -6,6 +6,23 @@
 
         async connectedCallback() {
             await super.connectedCallback();
+            this.props = {
+                modalRoot: document.querySelector('#modal-root')
+            }
+            this.shadowRoot.querySelector('#add-site-button').addEventListener('click', (_ev) => this.showAddModal());
+        }
+
+        showAddModal() {
+            const addSiteModal = document.createElement('text-input-modal');
+            addSiteModal.setModal('Enter a Category Name');
+            this.props.modalRoot.appendChild(addSiteModal);
+            addSiteModal.addEventListener('confirm', (ev) => this.onAddModalConfirm(ev));
+        }
+
+        onAddModalConfirm(ev) {
+            console.log(ev.detail);
+            //nvokerAPI.addSite(ev.detail);
+            //this.loadSites();
         }
     }
 
