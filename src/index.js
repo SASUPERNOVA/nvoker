7c68517b8b77f1d540591ec03034a357465cb306
SASUPERNOVA <agentrip@gmail.com>
2021-07-22 07:44:08 +0300

    Add add-site event handler and capturePage function in index.jh

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 const fs = require('fs');
 const path = require('path');
+const { encodeOSString } = require('./utils/os_string');
 
 if (require('electron-squirrel-startup')) {
   app.quit();
@@ -82,3 +83,62 @@ ipcMain.handle('load-file', async (_event, fileName) => {
     }
   }
 });
+
+ipcMain.handle('add-site', async (event, category, url) => {
+  try {
+    const linkPath = path.join(__dirname, 'userData', 'Links.json');
+
+    let data = fs.readFileSync(linkPath);
+    data = JSON.parse(data);
+    const title = await capturePage(category, url);
+
+    data[category][url] = title;
+
+    fs.writeFileSync(linkPath, JSON.stringify(data, null, ' '));
+  }
+  catch (err) {
+    throw err;
+  }
+});
+
+async function capturePage(category, url) {
+  const categoryPath = path.join(__dirname, 'userData', 'categories', category);
+  const snapshot = {
+        width: 800,
+        height: 600,
+        timeout: 0
+      };
+  const { timeout, width, height } = snapshot;
+  let browser = new BrowserWindow({
+    width,
+    height,
+    show: false,
+    webPreferences: {
+      offscreen: true // Necessary for rendering elements such as YouTube preview images
+    }
+  });
+
+  try {
+    browser.webContents.setAudioMuted(true);
+    await browser.loadURL(url);
+    await new Promise(resolve => setTimeout(resolve, timeout));
+    const image = await browser.webContents.capturePage();
+    // Allow special characters to be used in file name
+    const outputName = encodeOSString(browser.webContents.getTitle());
+    fs.mkdirSync(categoryPath, {recursive: true});
+    fs.writeFileSync(path.join(categoryPath, `${outputName}.png`), image.toPNG());
+    browser.close();
+    return outputName;
+  }
+  catch (err) {
+    if (err.code === 'ERR_INVALID_URL') {
+      new Notification({
+        title: "URL Capture Failed",
+        body: `Could not connect to ${url}`
+      }).show();
+    }
+    else {
+      throw err;
+    }
+  }
+}
