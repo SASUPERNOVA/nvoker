f351e4a4fb91a81da110c60e0fa6ccef71c963bb
SASUPERNOVA <agentrip@gmail.com>
2021-07-23 05:27:26 +0300

    Add onAddSitePress, onImageLinkClick, showRemoveModal, onRemoveModalConfirm, onRemoveConfirm, and onRemoveCancel methods in SiteView component

--- src/components/SiteView/SiteView.js
+++ src/components/SiteView/SiteView.js
@@ -36,7 +36,66 @@
                 const imageLink = document.createElement('image-link');
                 this.props.siteViewGrid.appendChild(imageLink);
                 imageLink.setImageLink(url, `userData/Links/${category}/${new URL(url).hostname}.png`, title, title);
+                imageLink.addEventListener('mousedown', (ev) => this.onAddSitePress(ev));
+                imageLink.addEventListener('click', (ev) => this.onImageLinkClick(ev));
             }
+        }
+
+        onAddSitePress (ev) {
+            const {target} = ev;
+        
+            const cancelTimeout = () => {
+                clearTimeout(holdTimeout);
+                target.removeEventListener('mouseup', cancelTimeout);
+                target.removeEventListener('mouseleave', cancelTimeout);
+            }
+        
+            const holdTimeout = setTimeout(() => {
+                this.showRemoveModal();
+            }, 1000);
+        
+            target.addEventListener('mouseup', cancelTimeout);
+            target.addEventListener('mouseleave', cancelTimeout);
+        }
+
+        onImageLinkClick(ev) {
+            if (this.props.modalRoot.children.length != 0) {
+                ev.target.classList.toggle('delete-site-selected');
+                return;
+            }
+            const url = ev.target.getLink();
+            console.log(url);
+        }
+
+        showRemoveModal() {
+            const removeCategoryModal = document.createElement('selection-modal');
+            this.props.modalRoot.appendChild(removeCategoryModal);
+            removeCategoryModal.addEventListener('confirm', (_ev) => this.onRemoveModalConfirm());
+            removeCategoryModal.addEventListener('cancel', (_ev) => this.onRemoveCancel());
+        }
+
+        onRemoveModalConfirm() {
+            const confirmationModal = document.createElement('confirmation-modal');
+            this.props.modalRoot.appendChild(confirmationModal);
+            confirmationModal.setModal('Delete Sites', 'Are you sure you want to delete the selected sites?');
+            confirmationModal.setActions(() => this.onRemoveConfirm(), () => this.onRemoveCancel());
+        }
+
+        onRemoveConfirm() {
+            const sites = Array.from(this.props.siteViewGrid.children)
+            .filter((element) => element.classList.contains('delete-site-selected'))
+            .map((imageLink) => imageLink.getLink());
+            //nvokerAPI.removeCategories(categories);
+            //this.loadCategories();
+            console.log(sites);
+            this.props.modalRoot.children[0].remove();
+        }
+
+        onRemoveCancel() {
+            for (const site of Array.from(this.props.siteViewGrid.children)) {
+                site.classList.toggle('delete-site-selected', false);
+            }
+            this.props.modalRoot.children[0].remove();
         }
     }
 
