7460a66852ce4bc95eeefc7b8d14946e327f0fbc
SASUPERNOVA <agentrip@gmail.com>
2021-07-23 09:55:54 +0300

    Add missing Notification import in index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
+const { app, BrowserWindow, ipcMain, Notification } = require('electron');
 const fs = require('fs');
 const path = require('path');
 
